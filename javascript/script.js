ed76f65a4d6794f02e80d902f1dda5bddfcb9948
Cameron Chandler <webstormcam@gmail.com>
2023-05-08 18:25:09 -0400

    Connected the API

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,11 @@
-const apiKey = "Your API key"
-const endpoint = "https://www.googleapis.com/youtube/v3/search"
+const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
+const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}`
 const channelId = "Replace with channel ID"
+
+
+
+fetch(endpoint)
+.then(res=>res.json())
+.then(data=>{
+    console.log(data)
+})
