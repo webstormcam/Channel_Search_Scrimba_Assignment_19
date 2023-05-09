a7ab75322d205135dfd59382db85266f99fe5325
Cameron Chandler <webstormcam@gmail.com>
2023-05-09 04:53:36 -0400

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
-const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&order=date&maxResults=10&channelId=UCrTNhL_yO3tPTdQ5XgmmWjA&q=Bat&type=video`
+const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&&maxResults=10&channelId=UCrTNhL_yO3tPTdQ5XgmmWjA&q="Star Trek"&type=video`
 const channelId = "Replace with channel ID"
 
 
