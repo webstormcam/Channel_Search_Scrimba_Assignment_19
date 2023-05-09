a45b7296877e78d508341eef373bffffca8f2802
Cameron Chandler <webstormcam@gmail.com>
2023-05-08 21:08:08 -0400

    Got red Letter Medias channelID

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
-const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}`
+const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&order=date&maxResults=10&channelId=UCrTNhL_yO3tPTdQ5XgmmWjA`
 const channelId = "Replace with channel ID"
 
 
