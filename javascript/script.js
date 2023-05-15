e7515f6acfaf733d25bb95fc8907605ddc69c085
Cameron Chandler <webstormcam@gmail.com>
2023-05-15 18:44:06 -0400

    Got all the channels to pull content with their IDS

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,18 @@
-// const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
-// const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&&maxResults=10&channelId=UCrTNhL_yO3tPTdQ5XgmmWjA&q="Avengers"&type=video`
-// const channelId = "Replace with channel ID"
 
 
 
-// fetch(endpoint)
-// .then(res=>res.json())
-// .then(data=>{
-//     console.log(data)
-// })
+
+const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
+const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&&maxResults=10&channelId=UCoLrcjPV5PbUrUyXq5mjc_A&q="Blue"&type=video`
+const channelId = "Replace with channel ID"
+
+//This is for finding the channel
+// const endpoint = `https://www.googleapis.com/youtube/v3/channels?key=${apiKey}&part=snippet,id&forUsername=MLB`
+
+
+
+fetch(endpoint)
+.then(res=>res.json())
+.then(data=>{
+    console.log(data)
+})
