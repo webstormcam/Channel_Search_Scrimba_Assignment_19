859efa008cdb39d1304f423f6207b597671e31fe
Cameron Chandler <webstormcam@gmail.com>
2023-05-15 22:43:36 -0400

    Building out the logic now

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,25 @@
+const userInput = document.getElementById('userInput')
+const enterButton = document.getElementById('enterButton')
+const optionDrop = document.getElementById('options')
+enterButton.addEventListener('click',searchFunction)
 
 
-
-
-const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
-const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&&maxResults=6&channelId=UCcoDpbA4c9HQY0AjKHSlKLg&q="anime"&type=video`
-const channelId = "Replace with channel ID"
+// const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
+// const endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&&maxResults=6&channelId=UCcoDpbA4c9HQY0AjKHSlKLg&q="anime"&type=video`
+// const channelId = "Replace with channel ID"
 
 //This is for finding the channel
 // const endpoint = `https://www.googleapis.com/youtube/v3/channels?key=${apiKey}&part=snippet,id&forUsername=Joel`
 
 
 
-fetch(endpoint)
-.then(res=>res.json())
-.then(data=>{
-    console.log(data)
-})
+// fetch(endpoint)
+// .then(res=>res.json())
+// .then(data=>{
+//     console.log(data)
+// })
+
+
+function searchFunction(){
+    
+}
