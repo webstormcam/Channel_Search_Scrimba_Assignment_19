da57abbf002b91f6847779ad578e8298b06c90be
Cameron Chandler <webstormcam@gmail.com>
2023-05-18 05:15:44 -0400

    Got searched videos to appear

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 const userInput = document.getElementById('userInput')
 const enterButton = document.getElementById('enterButton')
 const optionDrop = document.getElementById('youtubeChannels')
+const videoOptions = document.getElementById('videoOptions')
 enterButton.addEventListener('click',searchFunction)
 const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
 // let endpoint = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet,id&&maxResults=6&channelId=UCcoDpbA4c9HQY0AjKHSlKLg&q="anime"&type=video`
@@ -15,6 +16,7 @@ const apiKey = "AIzaSyDVdXSdZC0Ilhfo5vaJThktHwVhuOqz220"
 
 
 function searchFunction(){
+    videoOptions.innerHTML =""
     const searchText = userInput.value
     const selectedOption = optionDrop.value
     console.log(searchText,selectedOption)
@@ -23,8 +25,19 @@ function searchFunction(){
     fetch(endpoint)
     .then(res=>res.json())
     .then(data=>{
-    console.log(data.items)
-    //work with the length of data items
+    for(video of data.items){
+        const newVideo = `<div class="movie-element">
+        <a class="video-links" target="_blank" href="https://www.youtube.com/watch?v=${video.id.videoId}" >
+        <div class="video-image">
+            <img class="video-thumbnail" src="${video.snippet.thumbnails.high.url}" alt="">
+        </div>
+        <div class="video-text">
+            <h3>${video.snippet.title}</h3>
+        </div>
+    </a>
+    </div>`
+    videoOptions.innerHTML += newVideo
+    }
 })
    
 }
